95dd80229e424f0b47caa23787a13f1c0306b5aa
VadimKrbk <165938104+VadimKrbk@users.noreply.github.com>
2024-04-03 20:15:23 +0300

    Add files via upload

---
+++ kalk/src/calculator.java
@@ -0,0 +1,210 @@
+
+//import java.util.Scanner;
+//
+//class Main {
+//    public static void main(String[] args) throws Exception {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Введите выражение: ");
+//        String qwe = sc.nextLine();// выражение 1+2
+//        String result = calc(qwe);          //сюда записывается результат вычисления// в этот момент идет обращение к методу кальк и// в переменную инпут вставляется то выражение которое пришло к нам со сканера
+//        System.out.println(result);
+//    }
+//
+//
+//    public static String calc(String input) throws Exception {
+//        String[] s = input.split(" ");  // разбиваем массив в зависимости от оператора
+//
+//
+//
+//        if (s.length != 3) {
+//            throw new Exception("trows Exceptions"); ////формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)
+//
+//
+//        }
+//        int a;
+//        int b;
+//        try {                               //проверям на ввод пользователя только целые числа
+//            a = Integer.parseInt(s[0]);
+//            b = Integer.parseInt(s[2]);
+//        } catch (NumberFormatException e) {
+//            throw new Exception("trows Exceptions");
+//        }
+//
+//
+//
+//        char op = s[1].charAt(0);           //извлекает первый символ из элемента массива S
+//        int result = 0;
+//
+//
+//
+//        if ((a < 1) || (a > 10) || (b < 1) || (b > 10)) {
+//            throw new Exception("trows Exceptions");     // ввод пользователем неподходящих чисел >1&&<10
+//        }
+//
+//
+//
+//        switch (op) {
+//            case '+':
+//                result = a + b;
+//                break;
+//            case '-':
+//                result = a - b;
+//                break;
+//            case '*':
+//                result = a * b;
+//                break;
+//            case '/':
+//                result = a / b;
+//                break;
+//            default:
+//                throw new Exception("trows Exceptions");     // проверка на неверно введенный символ
+//        }
+//
+//
+//        return String.valueOf(result);
+//    }
+//}
+
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String expression = sc.nextLine();
+        String result = calc(expression);
+        System.out.println(result);
+
+
+    }
+
+    public static String calc(String input) throws Exception {
+        String[] s = input.split(" ");
+        if (s.length != 3) {
+            throw new Exception("Неверный формат выражения");
+        }
+        boolean hasArabic = input.matches(".*[0-9].*");
+        boolean hasRoman = input.matches(".*[IVXLCDM].*");
+        if (hasArabic && hasRoman) {
+            throw new IllegalArgumentException("Используются одновременно разные системы счисления");
+        }
+
+        boolean isArabic = true;
+
+
+        if (!s[0].matches(".") && !s[2].matches(".")){
+            throw new Exception("Введено десятичное число");
+        }
+        if (s[0].contains(".") || s[2].contains(".")||s[0].contains(",") || s[2].contains(",")) {
+            throw new Exception("Введено десятичное число");
+        }
+
+        int a;
+        int b;
+
+        try {
+
+            a = Integer.parseInt(s[0]);
+            b = Integer.parseInt(s[2]);
+        } catch (NumberFormatException ex) {
+            isArabic = false;
+            a = romanToArabic(s[0]);
+            b = romanToArabic(s[2]);
+        }
+
+
+
+
+
+        char op = s[1].charAt(0);
+        if ((a < 0 || a > 10) || (b < 0 || b > 10)) {
+            throw new Exception("Числа должны быть в диапазоне от 1 до 10");
+        }
+
+        int result;
+        switch (op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                if (!isArabic && result <= 0) {
+                    throw new Exception("Результат не может быть отрицательным");
+                }
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                if (b == 0) {
+                    throw new Exception("Деление на ноль");
+                }
+                result = a / b;
+                break;
+            default:
+                throw new Exception("Недопустимый оператор");
+        }
+
+        if (isArabic) {
+            return String.valueOf(result);
+        } else {
+            if (result <= 0) {
+                throw new Exception("Результат не может быть отрицательным");
+            }
+            return arabicToRoman(result);
+        }
+    }
+
+
+
+    public static int romanToArabic(String input) {
+        Map<Character, Integer> romanNumerals = new HashMap<>();
+        romanNumerals.put('I', 1);
+        romanNumerals.put('V', 5);
+        romanNumerals.put('X', 10);
+        romanNumerals.put('L', 50);
+        romanNumerals.put('C', 100);
+        romanNumerals.put('D', 500);
+        romanNumerals.put('M', 1000);
+
+        int result = 0;
+        int previousValue = 0;
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            int currentValue = romanNumerals.get(input.charAt(i));
+            if (currentValue < previousValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            previousValue = currentValue;
+        }
+        return result;
+    }
+
+
+    public static String arabicToRoman(int number) {
+        if (number < 1 || number > 3999) {
+            throw new IllegalArgumentException("Число должно быть в диапазоне от 1 до 3999");
+        }
+
+        String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] arabicValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        StringBuilder roman = new StringBuilder();
+        int remaining = number;
+
+        for (int i = 0; i < arabicValues.length; i++) {
+            while (remaining >= arabicValues[i]) {
+                roman.append(romanSymbols[i]);
+                remaining -= arabicValues[i];
+            }
+        }
+        return roman.toString();
+    }
+}
+
+
